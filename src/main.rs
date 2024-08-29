56615baaa3570b0ca062976fdf3993f48d254c07
Fabien TAVERNIER <fabien.tavernier@oclock.io>
2024-08-29 14:25:39 +0200

    3. action plan

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+/// Étapes :
+///
+///   1. récupérer les arguments de la commande
+///      → `cargo run -- README.md`
+///
+///   2. lire le fichier README.md
+///
+///   3. afficher le résultat
+
 fn main() {
     println!("Hello, world!");
 }
