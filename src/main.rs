225bd6fc615f9004dacc5e4412a6436479848841
Fabien TAVERNIER <fabien.tavernier@oclock.io>
2024-08-30 13:38:13 +0200

    9. add tests

--- src/main.rs
+++ src/main.rs
@@ -28,3 +28,48 @@ fn main() -> Result<(), String> {
 
     Ok(())
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn if_file_ok() {
+        let mut file = File::new(String::from("README.md"));
+        assert!(file.read().is_ok())
+    }
+
+    #[test]
+    fn if_file_is_not_found() {
+        let mut file = File::new(String::from("FAKE.txt"));
+        assert!(file.read().is_err())
+    }
+
+    #[test]
+    fn if_file_is_a_dir() {
+        let mut file = File::new(String::from("src"));
+        assert!(file.read().is_err())
+    }
+
+    #[test]
+    fn if_file_is_invalid() {
+        let mut file = File::new(String::from("rust.png"));
+        assert!(file.read().is_err())
+    }
+
+    #[test]
+    fn if_file_is_empty() {
+        let mut file = File::new(String::from("empty_file.txt"));
+        if let Ok(content) = file.read() {
+            assert!(content.is_empty())
+        }
+    }
+
+    #[test]
+    fn if_file_is_not_empty() {
+        let mut file = File::new(String::from("README.md"));
+        if let Ok(content) = file.read() {
+            assert!(content.contains('a'))
+        }
+    }
+}
