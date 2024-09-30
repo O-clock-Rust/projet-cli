11b9231133225f558d944cc81408b4dda6113768
Fabien TAVERNIER <100679956+FabienTavernier@users.noreply.github.com>
2024-09-30 14:53:25 +0200

    affiche le contenu ou l'erreur + modification des tests en foncion

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 use std::{env, process};
 
-use error::Error;
 use file::File;
 
 mod error;
@@ -8,26 +7,26 @@ mod file;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
+    // println!("{:#?}", args);
 
     if args.len() < 2 {
-        println!("Usage: {} <file>", args[0]);
+        println!(
+            "Erreur : vous devez fournir un chemin de fichier à lire → `$ cargo run -- sample.txt`",
+        );
         process::exit(1);
     }
 
     let file_path = &args[1];
-
-    let _ = read_file(file_path.to_string());
+    read_file(file_path.to_string());
 }
 
-fn read_file(file_path: String) -> Result<(), Error> {
-    let file = File::new(file_path.to_string());
+fn read_file(file_path: String) {
+    let file = File::new(file_path);
 
+    // récupérer le contenu
     match file.read() {
-        Ok(contents) => {
-            println!("{}", contents);
-            Ok(())
-        }
-        Err(err) => Err(err),
+        Ok(content) => println!("{}", content),
+        Err(e) => eprintln!("Erreur : {}", e),
     }
 }
 
@@ -36,22 +35,20 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_should_work_if_file_exists_and_is_not_empty() {
-        assert!(read_file("example.txt".to_string()).is_ok())
+    fn it_should_work() {
+        let file = File::new(String::from("example.txt"));
+        assert!(file.read().is_ok())
+    }
+
+    #[test]
+    fn it_should_not_work_if_file_not_exist() {
+        let file = File::new(String::from("not_exist.txt"));
+        assert!(file.read().is_err())
     }
 
     #[test]
     fn it_should_not_work_if_file_is_empty() {
-        assert!(read_file("empty.txt".to_string()).is_err())
-    }
-
-    #[test]
-    fn it_should_not_work_if_file_path_is_empty() {
-        assert!(read_file("".to_string()).is_err())
-    }
-
-    #[test]
-    fn it_should_not_work_if_file_path_does_not_exist() {
-        assert!(read_file("hello".to_string()).is_err())
+        let file = File::new(String::from("empty.txt"));
+        assert!(file.read().is_err())
     }
 }
